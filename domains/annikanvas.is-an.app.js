5b91c12185615843e1d5555090c5a71613a7c5f6
Annika <137914274+annikorp@users.noreply.github.com>
2023-08-22 00:12:03 +0800

    Create annikanvas.is-an.app.js

---
+++ domains/annikanvas.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'My own infinite canvas dashboard, for my personal endeavors and for projects with my clients', 
+  domain: 'is-an.app',
+  subdomain: 'annikanvas', 
+ owner: {
+    email: 'annikodes.dev@skiff.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+});
